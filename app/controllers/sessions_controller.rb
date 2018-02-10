6370d23f55d43037dd18f922f46a538e50064705
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-10 10:37:26 -0500

    third

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
 
 
     session = GoogleDrive.login_with_oauth(request.env["omniauth.auth"].credentials.token)
-    ws = session.spreadsheet_by_url("https://docs.google.com/spreadsheets/d/1smypkyAz4STrKO4Zkos5Z4UPUJKvvgIza32LnlQ7OGw/edit").worksheets[0]
+    # ws = session.spreadsheet_by_url("https://docs.google.com/spreadsheets/d/1smypkyAz4STrKO4Zkos5Z4UPUJKvvgIza32LnlQ7OGw/edit").worksheets[0]
     #Gets content of A2 cell
-    p ws[2, 1]
+    # p ws[2, 1]
     redirect_to root_path
   end
 
